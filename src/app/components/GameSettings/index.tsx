10f8df4c6016ad3fc9e9929850c000cea59f44ce
Ron Arr <rinat.r.abzalov@gmail.com>
2019-11-14 23:17:50 +0300

    Styles

--- src/app/components/GameSettings/index.tsx
+++ src/app/components/GameSettings/index.tsx
@@ -3,23 +3,36 @@ import * as React from 'react';
 import { classNameBuilder } from 'services/className';
 import { DropdownMenu } from 'ui/controls/DropdownMenu';
 
+const TEAM_BASE_TITLE = 'Количество команд';
+const LEVEL_BASE_TITLE = 'Уровень';
 const cn = classNameBuilder('game-settings');
 
 export const GameSettings: React.FC = () => {
-    const baseTitle = 'Количество команд';
     const [teamsCnt, setTeamsCount] = React.useState(2);
-    const [title, setTitle] = React.useState(`${baseTitle}: ${teamsCnt}`);
+    const [teamTitle, setTeamTitle] = React.useState(`${TEAM_BASE_TITLE}: ${teamsCnt}`);
+    const [level, setLevel] = React.useState('Простой');
+    const [levelTitle, setLevelTitle] = React.useState(`${LEVEL_BASE_TITLE}: ${level}`);
 
-    const items = [2, 3, 4, 5, 6];
+    const teamsCount = [2, 3, 4, 5, 6];
+    const levels = ['Простой', 'Нормальный', 'Сложный'];
     return (
         <div className={cn()}>
             <DropdownMenu
-                title={title}
-                items={items}
+                title={teamTitle}
+                items={teamsCount}
                 closeOnSelect={true}
                 onSelect={index => {
-                    setTeamsCount(items[index]);
-                    setTitle(`${baseTitle}: ${items[index]}`);
+                    setTeamsCount(teamsCount[index]);
+                    setTeamTitle(`${TEAM_BASE_TITLE}: ${teamsCount[index]}`);
+                }}
+            />
+            <DropdownMenu
+                title={levelTitle}
+                items={levels}
+                closeOnSelect={true}
+                onSelect={index => {
+                    setLevel(levels[index]);
+                    setLevelTitle(`${LEVEL_BASE_TITLE}: ${levels[index]}`);
                 }}
             />
         </div>
