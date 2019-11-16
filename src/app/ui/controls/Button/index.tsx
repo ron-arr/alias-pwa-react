6c254f2d2dd378e72115b9c9480bdf8cf4aa648a
Ron Arr <rinat.r.abzalov@gmail.com>
2019-11-16 14:21:40 +0300

    Add colors

--- src/app/ui/controls/Button/index.tsx
+++ src/app/ui/controls/Button/index.tsx
@@ -2,16 +2,21 @@ import './style.scss';
 import * as React from 'react';
 import { classNameBuilder } from 'als-services/className';
 
+type TButtonType = 'primary' | 'secondary';
+
 interface IProps {
     text: string;
+    type?: TButtonType;
     onAction: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const cn = classNameBuilder('button');
 
-export const Button: React.FC<IProps> = ({ text, onAction }: IProps) => {
+export const Button: React.FC<IProps> = ({ text, type, onAction }: IProps) => {
+    type = type || 'primary';
+
     return (
-        <button className={cn()} onClick={onAction}>
+        <button className={cn('', { [type]: true })} onClick={onAction}>
             <span>{text}</span>
         </button>
     );
