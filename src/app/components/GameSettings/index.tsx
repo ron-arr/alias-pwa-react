a72863cf5697662d095e504845bb5579c0d5c5cb
Ron Arr <rinat.r.abzalov@gmail.com>
2019-11-16 15:42:29 +0300

    Styles

--- src/app/components/GameSettings/index.tsx
+++ src/app/components/GameSettings/index.tsx
@@ -5,6 +5,7 @@ import { DropdownMenu } from 'als-ui/controls';
 
 const TEAM_BASE_TITLE = 'Количество команд';
 const LEVEL_BASE_TITLE = 'Уровень';
+const ROUND_DUR_BASE_TITLE = 'Время раунда';
 const cn = classNameBuilder('game-settings');
 
 export const GameSettings: React.FC = () => {
@@ -12,9 +13,12 @@ export const GameSettings: React.FC = () => {
     const [teamTitle, setTeamTitle] = React.useState(`${TEAM_BASE_TITLE}: ${teamsCnt}`);
     const [level, setLevel] = React.useState('Простой');
     const [levelTitle, setLevelTitle] = React.useState(`${LEVEL_BASE_TITLE}: ${level}`);
+    const [roundDur, setRoundDur] = React.useState('1 мин');
+    const [roundDurTitle, setRoundDurTitle] = React.useState(`${ROUND_DUR_BASE_TITLE}: ${roundDur}`);
 
     const teamsCount = [2, 3, 4, 5, 6];
     const levels = ['Простой', 'Нормальный', 'Сложный'];
+    const roundDurs = ['1 мин', '2 мин', '3 мин'];
     return (
         <div className={cn()}>
             <DropdownMenu
@@ -33,6 +37,15 @@ export const GameSettings: React.FC = () => {
                 onSelect={index => {
                     setLevel(levels[index]);
                     setLevelTitle(`${LEVEL_BASE_TITLE}: ${levels[index]}`);
+                }}
+            />
+            <DropdownMenu
+                title={roundDurTitle}
+                items={roundDurs}
+                closeOnSelect={true}
+                onSelect={index => {
+                    setRoundDur(roundDurs[index]);
+                    setRoundDurTitle(`${ROUND_DUR_BASE_TITLE}: ${roundDurs[index]}`);
                 }}
             />
         </div>
