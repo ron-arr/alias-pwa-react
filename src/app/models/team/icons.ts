a0f62b98b510bc2e7d2f4239ab8e1ea3fccbdecc
Ron Arr <rinat.r.abzalov@gmail.com>
2019-11-19 02:31:53 +0300

    Select team and results

---
+++ src/app/models/team/icons.ts
@@ -0,0 +1,38 @@
+import { AngelIcon, HipsterIcon, CoffeeManIcon, LgbtIcon } from 'als-icons/teams';
+
+export type TTeamIcon = { title: string; Icon: Function };
+
+export const teamIcons: TTeamIcon[] = [
+    {
+        title: 'Team 1',
+        Icon: AngelIcon,
+    },
+    {
+        title: 'Team 2',
+        Icon: HipsterIcon,
+    },
+    {
+        title: 'Team 3',
+        Icon: CoffeeManIcon,
+    },
+    {
+        title: 'Team 4',
+        Icon: LgbtIcon,
+    },
+    {
+        title: 'Team 5',
+        Icon: AngelIcon,
+    },
+    {
+        title: 'Team 6',
+        Icon: HipsterIcon,
+    },
+    {
+        title: 'Team 7',
+        Icon: CoffeeManIcon,
+    },
+    {
+        title: 'Team 8',
+        Icon: LgbtIcon,
+    },
+];
