3682196406e12b677aaf771186eaed2fb8c7f63f
Ron Arr <rinat.r.abzalov@gmail.com>
2019-12-01 22:46:15 +0300

    Add curtain

---
+++ src/app/components/Curtain/index.tsx
@@ -0,0 +1,9 @@
+import './styles.scss';
+import * as React from 'react';
+import { classNameBuilder } from 'als-services/className';
+
+const cn = classNameBuilder('curtain');
+
+export const Curtain: React.FC = () => {
+    return <div className={cn()}></div>;
+};
